2a554178a6f304dbcd60550ffcbb50f70389f3cf
Jeff Allen <jamaps@users.noreply.github.com>
2016-01-14 23:15:37 -0500

    Update batch_reproject.sh

--- batch_reproject.sh
+++ batch_reproject.sh
@@ -1,14 +1,15 @@
+# make an output folder
 mkdir "out"
+# loop shps and reproject into new output folder
 for i in *.shp
 do
 	# print info:
 	echo "----------------------------"
 	echo "$i"
-	# gdalinfo "$i"
-	echo "----------------------------"
 	# copy filename without extension:
 	f="${i%.*}"
 	echo "$f"
 	# assign new projection and put in output folder
 	ogr2ogr out/"$f".shp -t_srs "EPSG:4269" "$i"
 done
+echo "----------------------------"
