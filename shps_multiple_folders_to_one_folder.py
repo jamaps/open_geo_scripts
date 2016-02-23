8c81cb9ec34cfc1be0b1e47f668a94edc32cbdf7
Jeff Allen <jamaps@users.noreply.github.com>
2016-02-23 16:23:50 -0500

    Create shps_multiple_folders_to_one_folder.py

---
+++ shps_multiple_folders_to_one_folder.py
@@ -0,0 +1,36 @@
+import os
+from shutil import copyfile
+
+in_dir = r"PATH"
+out_dir =  r"PATH"
+
+s = d = x = p =0
+
+for root, dirs, files in os.walk(in_dir):
+    for file in files:
+        if file.endswith(".shp"):
+			fp = (os.path.join(root, file))
+			copyfile(fp, out_dir + slash + file)
+			s += 1
+for root, dirs, files in os.walk(in_dir):
+    for file in files:
+        if file.endswith(".dbf"):
+			fp = (os.path.join(root, file))
+			copyfile(fp, out_dir + slash + file)
+			d += 1
+for root, dirs, files in os.walk(in_dir):
+    for file in files:
+        if file.endswith(".shx"):
+			fp = (os.path.join(root, file))
+			copyfile(fp, out_dir + slash + file)
+			x += 1
+for root, dirs, files in os.walk(in_dir):
+    for file in files:
+        if file.endswith(".prj"):
+			fp = (os.path.join(root, file))
+			copyfile(fp, out_dir + slash + file)
+			p += 1
+			
+print s
+print d
+print x
