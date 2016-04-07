7ea32c06d06727af36f217f030de87c4db7f04f9
Jeff Allen <jamaps@users.noreply.github.com>
2016-04-07 16:27:22 -0400

    Create merge_geojsons.py

---
+++ merge_geojsons.py
@@ -0,0 +1,39 @@
+# merging multiple geojsons into one file
+# only tested with same geom type (eg point) and properties fields
+
+import os
+import time
+import json
+from subprocess import call
+from pprint import pprint
+
+start_time = time.time()
+
+geojsons_in_folder = "FOLDER_NAME"
+out_folder = "FOLDER_NAME"
+slash = "//"
+
+features_list = []
+for root, dirs, files in os.walk(geojsons_in_folder):
+    for gj in files:
+        if gj.endswith(".geojson"):
+            gj_path = geojsons_in_folder + slash + gj
+            print gj_path
+            with open(gj_path) as data_file:
+                data = json.load(data_file)
+                x = data["features"]
+                features_list = features_list + x
+
+merged_geojson = {
+    "type":"FeatureCollection",
+    # optional crs
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" }
+    },
+    "features": features_list
+}
+
+with open(out_folder + slash + "merge_test.geojson", 'w') as fp:
+    json.dump(merged_geojson, fp)
+
+print("--------------"
+print("%s seconds" % (time.time() - start_time))
