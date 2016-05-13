49c713043be131f2c7ddd18addfbec59c9bb3e90
Jeff Allen <jamaps@users.noreply.github.com>
2016-05-12 20:25:39 -0400

    Create shp_to_geojson_batch.sh

---
+++ shp_to_geojson_batch.sh
@@ -0,0 +1,15 @@
+
+mkdir "geojsons"
+for i in *.shp
+do
+	# print info:
+	echo "----------------------------"
+	echo "$i"
+	# copy filename without extension:
+	f="${i%.*}"
+	echo "$f"
+	# convert to shp into output folder
+	ogr2ogr -f "GeoJSON" "geojsons/$f.geojson" "$i"
+  echo "----------------------------"
+done
+cd ..
