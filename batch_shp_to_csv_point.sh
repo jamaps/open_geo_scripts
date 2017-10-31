e9036b310cebf6cec8efd961ca62940cebf55cd2
Jeff Allen <jamaps@users.noreply.github.com>
2017-10-31 18:17:50 -0400

    Create batch_shp_to_csv_point.sh

---
+++ batch_shp_to_csv_point.sh
@@ -0,0 +1,13 @@
+# converts points to XY columns
+
+cd in_folder
+mkdir out_folder
+
+for i in *.shp
+do
+	echo "----------------------------"
+  echo i
+  f="${i%.*}"
+	echo "$f"
+  ogr2ogr -f CSV out_folder/"$f".csv "$i" -lco GEOMETRY=AS_XY
+done
