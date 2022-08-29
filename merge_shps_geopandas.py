e1c3fb03441c708acc7561cc34e4f01562c6e9a5
Jeff Allen <jamaps@users.noreply.github.com>
2022-08-29 11:07:35 -0400

    Create merge_shps_geopandas.py

---
+++ merge_shps_geopandas.py
@@ -0,0 +1,26 @@
+# merges all shps in a directory
+# originally built for 1971 built up areas created by Stats Canada (each CMA to one Canada-wide file)
+
+import pandas as pd
+import geopandas as gpd
+import os
+
+folder = "data/built-up/"
+out_name = "built_up_1971.shp"
+
+files = os.listdir(folder)
+
+shps = []
+for file in files:
+    if file.endswith(".shp"):
+        shps.append(file)
+
+gdfs = []
+for shp in shps:
+    gdf = gpd.read_file(folder + shp)
+    # optional filter columns
+    gdf = gdf[["Code","CMA_RMR","geometry"]]
+    gdfs.append(gdf)
+
+gdfs = pd.concat(gdfs)
+gdfs.to_file(folder + out_name)
